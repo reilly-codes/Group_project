0db0e8f521cbc50966faeec678e26fadac46cf83
Reilly-codefreak <jroduory@gmail.com>
2020-08-06 09:02:41 +0300

    Button functionality

---
+++ js/init-firebase.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBLinxxMUvxTqRyye-z9MVHhUpYKxHTjqw",
+    authDomain: "shopping-list-d33a8.firebaseapp.com",
+    databaseURL: "https://shopping-list-d33a8.firebaseio.com",
+    projectId: "shopping-list-d33a8",
+    storageBucket: "shopping-list-d33a8.appspot.com",
+    messagingSenderId: "247425796518",
+    appId: "1:247425796518:web:fbdfb6bd70ae843ab5a767",
+    measurementId: "G-H0XKKX47YC"
+  };
+
+firebase.initializeApp(firebaseConfig);
+// firebase.auth();
+// firebase.analytics();
+// firebase.database();
